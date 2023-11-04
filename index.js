8c9fa4100228cddb47206fe0093eaa6631be4dc3
homemmakako <homemmakako@gmail.com>
2023-11-03 21:52:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
   import { inspect } from "util";
-  import MyFile, { parti } from "./libs/my-file.js";
+  import Default, { parti } from "./libs/my-file.js";
 
   function hello (name) {
     console.log("Hello " + name + "!");
-    console.log("Default import", MyFile);
+    console.log("Default import", Default);
     console.log("Partial import", parti);
   }
 
