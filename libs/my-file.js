044189e92c3139611d0a224c91a827231b7ca4d1
homemmakako <homemmakako@gmail.com>
2023-11-07 20:58:04 -0300

    Update my-file.js

--- libs/my-file.js
+++ libs/my-file.js
@@ -2,7 +2,7 @@ import { rnd } from "../math/rnd.js";
 import { inspect } from "util";
 
 const defa = inspect({
-  ["default string is an object"]: rnd().toString();
+  ["default string is an object"]: rnd().toString(),
 }, false, null, true);
 const parti = "partial string " + rnd().toString();
 
