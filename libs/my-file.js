f4995ff31a91de80a34058b0178325316adfd917
homemmakako <homemmakako@gmail.com>
2023-11-23 21:18:16 -0300

    Update my-file.js

--- libs/my-file.js
+++ libs/my-file.js
@@ -1,10 +1,10 @@
-import { rnd } from "../math/rnd.js";
+import { rnd as random } from "../math/rnd.js";
 import { inspect as check } from "util";
 
 const defa = check({
-  ["default string is an object"]: rnd().toString(),
+  ["default string is an object"]: random().toString(),
 }, false, null, true);
-const parti = "partial string " + rnd().toString();
+const parti = "partial string " + random().toString();
 
 export { parti };
 
